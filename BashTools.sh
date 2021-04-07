fe4e4183eba550eb48f52df387002210c6ebcdbb
deep <pandeydeep9g@gmail.com>
2021-04-06 23:10:46 -0400

    Fix audio trimming

--- BashTools.sh
+++ BashTools.sh
@@ -1,4 +1,4 @@
 Edit music audio from to
 https://unix.stackexchange.com/questions/182602/trim-audio-file-using-start-and-stop-times
 
-ffmpeg -i file.mkv --ss 00:00:20 -to  00:00:40 -c copy file2.mkv
+ffmpeg -i file.mkv -ss 00:00:20 -to  00:00:40 -c copy file_to_save.mkv
